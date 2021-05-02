d488ea02c5b551cd5f077c0c8e7366a07de5bec4
Dionne Lie-Sam-Foek <dionnelie@xs4all.nl>
2021-05-02 11:20:59 +0200

    Added Color extension with commonly used colors.

---
+++ Sources/DLDSwiftUI/ColorHelpers.swift
@@ -0,0 +1,24 @@
+//
+//  NewHelper.swift
+//  
+//
+//  Created by Dionne Lie-Sam-Foek on 02/05/2021.
+//
+
+import SwiftUI
+
+@available(OSX 11.0, iOS 14.0, *)
+public extension Color {
+    
+    #if os(iOS)
+    static let systemBackground                 = Color(UIColor.systemBackground)
+    static let secondarySystemBackground        = Color(UIColor.secondarySystemBackground)
+    static let systemGroupedBackground          = Color(UIColor.systemGroupedBackground)
+    static let secondarySystemGroupedBackground = Color(UIColor.secondarySystemGroupedBackground)
+    #endif
+    
+    static let shadowBlack = Color.black.opacity(0.2)
+    static let halfBlack   = Color.black.opacity(0.5)
+    static let halfGray    = Color.gray.opacity(0.5)
+    static let halfWhite   = Color.white.opacity(0.5)
+}
