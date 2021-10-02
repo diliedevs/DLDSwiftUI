61e58b8f01adac537acd4abe2a3bafad77b7bf44
Dionne Lie-Sam-Foek <dionnelie@icloud.com>
2021-10-02 12:50:01 +0200

    Hopefully fixed Section Helpers obsoletion.

--- Sources/DLDSwiftUI/SectionHelpers.swift
+++ Sources/DLDSwiftUI/SectionHelpers.swift
@@ -8,12 +8,16 @@
 
 import SwiftUI
 
-@available(macOS, obsoleted: 12, message: "SwiftUI now has these same initializers.")
-@available(iOS, obsoleted: 15, message: "SwiftUI now has these same initializers.")
 public extension Section where Parent == Text, Content: View, Footer == EmptyView {
+    
+    @available(macOS, obsoleted: 12, message: "SwiftUI now has this same initializer.")
+    @available(iOS, obsoleted: 15, message: "SwiftUI now has this same initializer.")
     init<S: StringProtocol>(_ title: S, @ViewBuilder content: () -> Content) {
         self.init(header: Text(title), content: content)
     }
+    
+    @available(macOS, obsoleted: 12, message: "SwiftUI now has this same initializer.")
+    @available(iOS, obsoleted: 15, message: "SwiftUI now has this same initializer.")
     init(_ titleKey: LocalizedStringKey, @ViewBuilder content: () -> Content) {
         self.init(header: Text(titleKey), content: content)
     }
